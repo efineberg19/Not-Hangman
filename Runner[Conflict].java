025695829ea66807a396434212b0f72e559e0157
efineberg19 <33038372+efineberg19@users.noreply.github.com>
2018-02-26 17:33:48 -0500

    Update Runner[Conflict].java

--- Runner[Conflict].java
+++ Runner[Conflict].java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Runner here.
+ * Runs the game.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Beth Fineberg
+ * @version 1.0
  */
 public class Runner
 {
